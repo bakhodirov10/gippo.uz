a2edf6395966f532e9ab347c20155a59c100c0c5
Abdulloh <boxodirovabdulloh46@gmail.com>
2026-08-19 23:10:40 +0500

    boldie

--- frontend/src/app/dashboard/loading.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { SkeletonDashboard } from '@/components/ui/skeletons';
-
-export default function DashboardLoading() {
-  return <SkeletonDashboard />;
-}
--- frontend/src/app/doctors/[id]/loading.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { SkeletonDoctorProfile } from '@/components/ui/skeletons';
-
-export default function DoctorDetailLoading() {
-  return <SkeletonDoctorProfile />;
-}
